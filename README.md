94f07baec99ee95677407fb3ecb88a9c7eee08cd
Brian Ritchie <brianr@xeohealth.com>
2022-10-28 11:59:56 -0400

    Implement OAuth for Exchange Web Service

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # SplunkMailProcessor
 Processes mail messages from a Exchange/Office365 mailbox and ingests them as alerts into Splunk
+
+See these pages for setup within Office365/Azure/Exchange and explaination of auth code
+* https://learn.microsoft.com/en-us/exchange/client-developer/exchange-web-services/how-to-authenticate-an-ews-application-by-using-oauth
+* https://learn.microsoft.com/en-us/graph/auth-limit-mailbox-access
--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Configuration;
+using System.Net;
 using Microsoft.Exchange.WebServices.Data;
+using Microsoft.Identity.Client;
 
 namespace SplunkMailProcessor
 {
@@ -11,7 +13,9 @@ namespace SplunkMailProcessor
             var splunkApiKey = ConfigurationManager.AppSettings["Splunk.ApiKey"];
             var splunkCollectorUrl = ConfigurationManager.AppSettings["Splunk.Url"];
             var exchangeUser = ConfigurationManager.AppSettings["Exchange.User"];
-            var exchangePwd = ConfigurationManager.AppSettings["Exchange.Pwd"];
+            var appId = ConfigurationManager.AppSettings["Exchange.AppId"];
+            var clientSecret = ConfigurationManager.AppSettings["Exchange.ClientSecret"];
+            var tenantId = ConfigurationManager.AppSettings["Exchange.TenantId"];
             var exchangeUrl = ConfigurationManager.AppSettings["Exchange.Url"];
             var exchangeMsgLimit = int.Parse(ConfigurationManager.AppSettings["Exchange.MessagesPerPoll"]);
 
@@ -20,12 +24,36 @@ namespace SplunkMailProcessor
             Console.WriteLine("  Exchange Url: {0}", exchangeUrl);
             Console.WriteLine("  Splunk HTTP Event Collector Url: {0}", splunkCollectorUrl);
 
+            // See these pages for setup within Office365/Azure/Exchange and explaination of auth code
+            // https://learn.microsoft.com/en-us/exchange/client-developer/exchange-web-services/how-to-authenticate-an-ews-application-by-using-oauth
+            // https://learn.microsoft.com/en-us/graph/auth-limit-mailbox-access
+
+            // Using Microsoft.Identity.Client 
+            var cca = ConfidentialClientApplicationBuilder
+                .Create(appId)
+                .WithClientSecret(clientSecret)
+                .WithTenantId(tenantId)
+                .Build();
+
+            // The permission scope required for EWS access
+            var ewsScopes = new string[] { "https://outlook.office365.com/.default" };
+
+            // Make the token request
+            var authResult = cca.AcquireTokenForClient(ewsScopes).ExecuteAsync().Result;
+
+            // Use token to authenticate with exchange service
             var exchangeService = new ExchangeService(ExchangeVersion.Exchange2013_SP1)
             {
-                Credentials = new WebCredentials(exchangeUser, exchangePwd),
-                //service.TraceEnabled = true;
+                Credentials = new OAuthCredentials(authResult.AccessToken),
                 Url = new Uri(exchangeUrl)
             };
+
+            //Impersonate the mailbox to access.
+            exchangeService.ImpersonatedUserId = new ImpersonatedUserId(ConnectingIdType.SmtpAddress, exchangeUser);
+
+            // Include x-anchormailbox header
+            exchangeService.HttpHeaders.Add("X-AnchorMailbox", exchangeUser);
+
             var mailAdapter = new ExchangeMailAdapter(exchangeService);
             var publisher = new SplunkAlertPublisher(splunkCollectorUrl, splunkApiKey);
             var alertGenerators = new IAlertGenerator[] { new VerifyAttachmentAlertGenerator(), new DefaultAlertGenerator() };
